10b01185cae62e4315101d2434f65f6e4e9dd987
istvian <e.r94@outlook.cl>
2021-10-14 15:19:16 -0300

    funcional

--- js/app.js
+++ js/app.js
@@ -24,17 +24,18 @@ let menu = document.getElementById("menu");
 let link = Array.from(document.getElementsByClassName("link"));
 link.forEach(element => {
     element.onclick = function name(params) {
-        if (menu.style.display == "none") {
-            menu.style.display = "flex";
-        } else {
+        if (menu.className == "nav__links") {
+            menu.className = "menu__hide";
             menu.style.display = "none";
         }
     }
 });
 btn.onclick = () => {
-    if (menu.style.display == "none") {
+    if (menu.className == "menu__hide") {
         menu.style.display = "flex";
+        menu.className = "nav__links";
     } else {
+        menu.className = "menu__hide";
         menu.style.display = "none";
     }
 };
@@ -43,29 +44,69 @@ btn.onclick = () => {
 
 let region_selector = document.getElementById("region-selector");
 let city_selector = document.getElementById("city-selector");
+let f_nombre = document.getElementById("farmacia-nombre");
+let f_hora = document.getElementById("farmacia-hora");
+let f_direccion = document.getElementById("farmacia-direccion");
+let f_call = document.getElementById("farmacia-call");
+let f_geo = document.getElementById("farmacia-geo");
 let farmacias = new Array();
 let regiones = new Array();
-GetFarmacias();
 GetComunas();
-
+GetFarmacias();
+// CUANDO SE SELECCIONA UNA REGION
 region_selector.onchange = () => {
-    if (city_selector.children.length > 1) {
-        for (let i = 1; i < city_selector.length; i++) {
-            city_selector.removeChild(city_selector.childNodes[i]);
-        }
+    // ELIMINA LAS CIUDADES O COMUNAS
+    ClearFarmacia();
+    while (city_selector.hasChildNodes()) {
+        city_selector.removeChild(city_selector.childNodes[0]);
     }
-    for (let index = 0; index < region_selector.childElementCount; index++) {
-        console.log(regiones[region_selector.value - 1].region);
-        if (region_selector.childNodes[index].textContent == regiones[region_selector.value - 1].region) {
-            for (let i = 0; i < regiones[region_selector.value - 1].comunas.length; i++) {
-                let option = document.createElement("option");
-                option.innerText = regiones[region_selector.value - 1].comunas[i];
-                option.value = i;
+    // SI NO ELIGE NINGUNA REGION OCULTA LAS CIUDADES
+    if (region_selector.value == 0) {
+        city_selector.style.display = "none";
+    } else {
+        // LO MUESTRA, AGREGA LA OPCION 0
+        city_selector.style.display = "flex";
+        let option = new Option("Selecciona una ciudad", 0);
+        city_selector.appendChild(option);
+        // AGREGA LAS CIUDADES EN QUE CORRESPONDEN CON LA REGION
+        // let result = regiones.find(e => e.numero == region_selector.value);
+        // for (let i = 0; i < result.comunas.length; i++) {
+        //     let option = new Option(result.comunas[i], i + 1);
+        //     city_selector.appendChild(option);
+        // }
+        let result = farmacias.filter(e => e.fk_region == region_selector.value);
+        if (result.length > 0) {
+            for (let i = 0; i < result.length; i++) {
+                let option = new Option(String(result[i].comuna_nombre).toUpperCase(), i + 1);
                 city_selector.appendChild(option);
             }
+        } else {
+            let option = new Option("Sin datos", "0");
+            city_selector.appendChild(option);
         }
+
     };
 };
+
+function ClearFarmacia() {
+    f_nombre.textContent = "Nombre: ";
+    f_hora.textContent = `Horario: `;
+    f_direccion.textContent = `Dirección: `;
+    f_call.href = `#`;
+    f_geo.href = `#`;
+}
+
+city_selector.onchange = () => {
+    ClearFarmacia();
+    let result = farmacias.find(e => e.comuna_nombre == city_selector.childNodes[city_selector.value].textContent.toUpperCase());
+    if (result != null) {
+        f_nombre.textContent = `Nombre: ${result.local_nombre}`;
+        f_hora.textContent = `Horario: ${String(result.funcionamiento_hora_apertura).slice(0,5)} a ${String(result.funcionamiento_hora_cierre).slice(0,5)}`
+        f_direccion.textContent = `Dirección: ${result.local_direccion}`;
+        f_call.href = `callto:${result.local_telefono}`;
+        f_geo.href = `https://maps.google.com/?q=${result.local_lat},${result.local_lng}`;
+    }
+}
 
 function SetFarmacia(json) {
     farmacias = JSON.parse(json);
